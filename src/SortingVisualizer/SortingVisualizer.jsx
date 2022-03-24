047d024c57f6ba9da3f471b255f49c2b50b171fd
Guangyu Yang <dukeyang7@gmail.com>
2022-03-24 19:55:52 +0800

    update the UI with material UI

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,24 +2,38 @@ import React from "react";
 import "./SortingVisualizer.css"
 import { getMergeAni } from "../SortingAlgos/mergeSort";
 import { getBubbleAni } from "../SortingAlgos/bubbleSort";
+import Slider from '@mui/material/Slider';
+import { Box, Button, ButtonGroup, SvgIcon, Typography } from '@mui/material';
+import BarChart from "@mui/icons-material/BarChart";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import shadows from "@mui/material/styles/shadows";
+
+let size = 20;
+let show = false;
 
 export class SortingVisualizer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             history: [{
-                nums: Array(35).fill(0)
+                nums: Array(size).fill(0)
             }],
             step: 0,
-            animations: [[0, 1], [2, 3], [4, 5]],
+            animations: [],
             sorted: false,
         }
     }
 
     resetArray() {
-        const array = Array(35).fill(0);
-        for (let i = 0; i < 35; i++) {
+        const array = Array(size).fill(0);
+        for (let i = 0; i < size; i++) {
             array[i] = randomIntFromInterval(5, 500);
+            const existedBar = document.getElementById("bar" + i);
+            if (existedBar) {
+                existedBar.style.visibility = "hidden";
+            }
         }
 
         this.setState({
@@ -30,11 +44,12 @@ export class SortingVisualizer extends React.Component {
     }
 
     display() {
-
-        for (let i = 0; i < 35; i++) {
+        show ? show = false : show = true;
+        for (let i = 0; i < size; i++) {
             const val = document.getElementById("bar" + i);
             if (val.style.visibility === "hidden") {
                 val.style.visibility = "visible";
+
             } else {
                 val.style.visibility = "hidden";
             }
@@ -91,9 +106,6 @@ export class SortingVisualizer extends React.Component {
     }
 
 
-
-
-
     //TODO: create a new class called array bar, and add handclick function to it
     //so each time click on the bar the value of this bar will appear
     render() {
@@ -101,10 +113,24 @@ export class SortingVisualizer extends React.Component {
         const arr = current.nums.slice();
         return (
             <div>
-                <button onClick={()=>this.resetArray()}> {"generate array"} </button>
-                <button onClick={()=>this.display()}>{"display value"}</button>
-                <button onClick={()=>this.mergeSort()}>{"merge-sort"}</button>
-                <button onClick={()=>this.bubbleSort()}>{"bubble-sort"}</button>
+                <ButtonGroup variant="contained" aria-label="outlined primary button group" size="medium">
+                    <Button startIcon={<BarChart/>} onClick={()=> {this.resetArray()}}> Generate New Array </Button>
+                    <Button id="display-button" 
+                        onClick={() => {this.display()}}
+                        startIcon={show ? <VisibilityOffIcon/> : <VisibilityIcon/>}>
+                        Show Element value </Button>
+                    <Button startIcon={<PlayArrowIcon/>} onClick={()=>this.mergeSort()}>Demo MergeSort</Button>
+                    <Button startIcon={<PlayArrowIcon/>} onClick={()=>this.bubbleSort()}>Demo BubbleSort</Button>
+                </ButtonGroup>
+                <Box>
+                    <Typography id="input-slider" gutterBottom>
+                        Array Size
+                    </Typography>
+                    <Slider min={20} max={50} sx={{width: 300}} aria-label="Size" 
+                        valueLabelDisplay="auto" dots={true} 
+                        marks={[20, 25, 30, 35, 40, 45, 50]} 
+                        step={5} onChange={(_, value) => {size = value}}/>
+                </Box>
                 <div className="arr-container">
                     {arr.map((value, idx) => (<Bar value={value} key={idx} id={"bar" + idx} onClick={()=>this.handleClick(idx)}></Bar>))}
                 </div>
@@ -119,19 +145,25 @@ function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+
+const barStyle = {
+    marginLeft:`${Math.floor(10 / size)}%`,
+    marginRight:`${Math.floor(10 / size)}%`,
+}
+
 function Bar(props) {
 
     return (
-            <div className="arr-bar">
+            <div className="arr-bar" style={barStyle}>
             <div id={props.id} style={{visibility:"hidden", fontSize:"60%"}}>{props.value}</div>
-            <button  
+            <Box    
                     value={props.value} 
-                    onClick={()=>props.onClick()}
                     id = {"long" + props.id} 
-                    style={{height: `${props.value}px`, backgroundColor:"blue"}}>
+                    style={{height: `${props.value}px`, backgroundColor:"blue", width:`${25}px`}}>
                     
-            </button>
+            </Box>
             </div>
             );
 }
+
 
